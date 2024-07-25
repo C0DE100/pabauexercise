a44c5c4920e57eeee8365699fb0e3b25f3d60493
Aleksandar Jovanovski <jovanovski.a74@gmail.com>
2024-07-25 19:46:33 +0200

    change

--- index.php
+++ index.php
@@ -25,18 +25,36 @@ $matrix = [
     ["s"," "," "," ","C","-","-","-","+"]
 ];
 
-function followPath($matrix){
-    $startingPosition = getStartingPosition($matrix);
-    $currentPosition = $startingPosition;
+function followPath($matrix) {
+    $currentPosition = getStartingPosition($matrix);
     $previousPosition = "";
     $letters = "";
     $path = "";
 
-    while ($currentPosition !== 's'){
-        $nextPosition = getNextPosition($matrix,$currentPosition,$previousPosition);
-        
+    while ($currentChar !== 's') {
+        $currentChar = $matrix[$currentPosition[0]][$currentPosition[1]];
+        $path .= $currentChar;
+
+        if (isUpperCaseLetter($currentChar)) {
+            $letters .= $currentChar;
+        }
+
+        $nextPosition = getNextPosition($matrix, $currentPosition, $previousPosition);
+
+        // Stop if no movement is possible or reached the end
+        if ($nextPosition == $currentPosition) {
+            break;
+        }
+
+        // Update previous and current positions
+        $previousPosition = $currentPosition;
+        $currentPosition = $nextPosition;
     }
+
+    echo "Path: " . $path . "<br>";
+    echo "Letters: " . $letters . "<br>";
 }
+
 
 function getNextPosition($matrix, $currentPosition, $previousPosition = "") {
 
@@ -44,7 +62,7 @@ function getNextPosition($matrix, $currentPosition, $previousPosition = "") {
     $y = $currentPosition[1];
 
     //always move right from the starting position
-    if([$x, $y] === getStartingPosition($matrix)){
+    if ($previousPosition === "" && [$x, $y] === getStartingPosition($matrix)) {
         return [$x, $y + 1];
     }
 
@@ -71,13 +89,6 @@ function getNextPosition($matrix, $currentPosition, $previousPosition = "") {
 
 }
 
-echo "Current position given is: <br>";
-print_r([0,2]);
-echo "<br>";
-echo "Next position is: <br>";
-print_r(getNextPosition($matrix, [0,2],[0,1]));
-
-
 // Helper functions
 function getStartingPosition($matrix) {
     for ($i = 0; $i < count($matrix); $i++){
@@ -93,4 +104,26 @@ function isMatch($coordinates, $character) {
         return $coordinates === $character;
 }
 
+function isUpperCaseLetter($character) {
+    return ctype_upper($char);
+}
+
+followPath($matrix);
 ?>
+
+<!-- function followPath($matrix){
+    $startingPosition = getStartingPosition($matrix);
+    $currentPosition = $startingPosition;
+    $previousPosition = "";
+    $letters = "";
+    $path = "";
+
+    while ($currentPosition !== 's'){
+        $nextPosition = getNextPosition($matrix,$currentPosition,$previousPosition);
+
+        if (isUpperCaseLetter($nextPosition)) {
+            $letters .= $matrix[]
+        }
+
+    }
+} -->
